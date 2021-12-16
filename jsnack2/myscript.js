71a71195b1d68178fd2d114988907af4048eaad8
LOSAFABIO <fabio.losa95@gmail.com>
2021-12-16 17:28:55 +0100

    Sembra buono ma mi genera numeri infiniti

---
+++ jsnack2/myscript.js
@@ -0,0 +1,26 @@
+/*
+JSnack 2
+Fai inserire un numero, che chiameremo N, all’utente.
+Genera N array, ognuno formato da 
+10 numeri casuali tra 1 e 100.
+Ogni volta che ne crei uno, stampalo a schermo.
+*/
+
+
+//chidere numero
+let N = parseInt(prompt("Inserisci un numero"));
+
+
+for( let i=0; i< N; i++){
+    
+    let nArr = [];
+
+    for( let cont = 0; cont<10; i++){
+
+    nArr.push(Math.floor(Math.random()*100+1))
+    console.log(nArr);
+    }
+}
+
+
+//createElement
