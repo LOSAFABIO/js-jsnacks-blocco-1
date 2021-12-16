1d3950ff2f0a730da9b0711a750bb5047f4276b3
LOSAFABIO <fabio.losa95@gmail.com>
2021-12-16 17:09:05 +0100

    ok, tranne che non mi chiede più aggiungere numero

---
+++ jsnack1/myscript.js
@@ -0,0 +1,34 @@
+/*
+JSnack 1
+Crea un array vuoto e chiedi all’utente un numero 
+da inserire nell’array. Continua a chiedere i numeri 
+all’utente e a inserirli nell’array fino a quando 
+la somma degli elementi è minore di 50.
+*/
+
+
+//Arry vuoto
+let gruppoArr = [];
+
+//Contatore
+let contatore = 0
+
+//Somma
+let somma = 0
+//Prompt numero
+
+
+//somma < 50
+do{
+    let numero = parseInt(prompt ("Scrivi un numero"));
+    console.log(numero);
+
+    gruppoArr[contatore] = numero;
+    console.log(gruppoArr);
+
+    somma += gruppoArr[contatore];
+    console.log(somma);
+
+    contatore++;
+
+} while (gruppoArr < 50);
